bc7e794ff4f57536e4ea1182c75c1dd421e245ea
Himanshu Singh <himanshusingh1k@gmail.com>
2025-10-07 15:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const panels = document.querySelectorAll(".panel");
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    panels.forEach((panel) => panel.classList.remove("active"));
+    panel.classList.add("active");
+  });
+});
